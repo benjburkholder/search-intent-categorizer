375d0e61464c65b5250e993213d8cd3f76a95629
Ben <benjburkholder@gmail.com>
2019-03-10 16:57:28 -0400

    first commit

--- serp-intent-categorizer.py
+++ serp-intent-categorizer.py
@@ -1,5 +1,97 @@
 """
 This program will categorize queries by what SERP features are present for the query.
 This will be achieved via a variety of If statements.
+
+~ Informational
+~ Navigational
+~ Transactional
 """
+
+from lib.google_search_results import GoogleSearchResults
+
+api_key = open('serp-api-key.txt').read()
+
+downloadFile = 'serp-intent-results.csv'
+file = open(downloadFile, 'w')
+
+columnHead = 'Query,Search Intent Type\n'
+file.write(columnHead)
+
+with open('serp_intent_urls.txt') as content:
+    content = [line.rstrip('\n') for line in content]
+    content = [line.split(",") for line in content]
+
+# If user location can remain static, location can be hardcoded with a location.
+    for line in content:
+        params = {
+            "q": str(line[0]),
+            "location": f"{line[1]},{line[2]},United States",
+            "hl": "en",
+            "gl": "us",
+            "google_domain": "google.com",
+            "api_key": api_key,
+        }
+
+        query = GoogleSearchResults(params)
+        dictionary_results = query.get_dictionary()
+        finalquery = "{} {} {}".format(line[0], line[1], line[2])
+
+    # Information Search Intent
+        try:
+            answerbox = dictionary_results['answer_box']
+            paa = dictionary_results['related_questions']
+        except KeyError:
+            print('Not Informational Intent')
+        else:
+            print(f'{finalquery}, Information Search Intent')
+            file.write(f'{finalquery}, Information Search Intent\n')
+
+
+    # Navigational Search Intent
+        try:
+            knowledge_graph = dictionary_results['knowledge_graph']
+            site_links = dictionary_results['organic_results'][0]['sitelinks']
+        except KeyError:
+            print('Not Navigational Search Intent')
+        else:
+            print(f'{finalquery}, Navigational Search Intent')
+            file.write(f'{finalquery}, Navigational Search Intent\n')
+
+    # Transactional
+        try:
+            shopping_ads = dictionary_results['shopping_results']
+        except KeyError:
+            print('Not Transactional Intent')
+        else:
+            print(f'{finalquery}, Transactional Search Intent')
+            file.write(f'{finalquery}, Transactional Search Intent\n')
+
+    # Commercial Investigation
+        results = []
+        bottom_ads = ''
+
+        try:
+            ads = dictionary_results['ads']
+            answerbox = dictionary_results['answer_box']
+            paa = dictionary_results['related_questions']
+            organic_results = dictionary_results['organic_results']
+            for pos in organic_results:
+                results.append(1)
+
+            if len(results) > 10:
+                bottom_ads = True
+        except KeyError:
+            print('Not Commercial Investigation Intent')
+        else:
+            if bottom_ads != '':
+                print(f'{finalquery}, Commercial Investigation')
+                file.write(f'{finalquery}, Commercial Investigation\n')
+
+file.close()
+
+
+
+
+
+
 
